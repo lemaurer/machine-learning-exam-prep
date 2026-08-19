7e33185a8aeaed4e590d9f56e047399afb68e508
Leif Maurer <leif.maurer@gmail.com>
2026-08-19 18:48:59 +0200

    Import every exam explicitly in production bundle

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import "../data/fs23-additions";
 import "../data/hs22-additions";
 import "../data/fs22-additions";
 import "../data/hs21-additions";
+import "../data/fs21-additions";
 import { ExamPrepApp } from "../app/ExamPrepApp";
 import { installMultiFigureSupport } from "../app/multi-figure";
 import { installWeightedScoringPresentation } from "../app/weighted-scoring";
