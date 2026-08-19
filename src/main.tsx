c8b6100d908c4c24a98bf85287e833dc709ae37a
Leif Maurer <leif.maurer@gmail.com>
2026-08-19 13:07:07 +0200

    Load FS22 exam in app

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "../data/fs24-additions";
 import "../data/hs23-additions";
 import "../data/fs23-additions";
 import "../data/hs22-additions";
+import "../data/fs22-additions";
 import { ExamPrepApp } from "../app/ExamPrepApp";
 import { installMultiFigureSupport } from "../app/multi-figure";
 import "../app/session-tight.css";
